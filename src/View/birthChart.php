983e3979b2e111493480bb84af6131ecdb78c367
Alex <21338787@studentmail.ul.ie>
2024-03-05 13:18:09 +0000

    birth chart generated from session data

--- src/View/birthChart.php
+++ src/View/birthChart.php
@@ -1,8 +1,11 @@
 <html>
     <div class="generalText">
         <body>
+            <?php 
+                session_start();
+            ?>
             <div id="birthChart"></div>
-            <script src="https://unpkg.com/@astrodraw/astrochart"></script>
+            <script src="https://unpkg.com/@astrodraw/astrochart@3.0.2/dist/astrochart.js"></script>
             <script src="../lib/js_astro-master/src/astronomy.js"></script>
             <script src="../lib/js_astro-master/src/cuspcal.js"></script>
             <script src="../lib/js_astro-master/src/geodata.js"></script>
@@ -13,15 +16,30 @@
             <script type="text/javascript">
                 //SOURCE FOR js_astromaster https://github.com/astsakai/js_astro 
                 //TODO: add something manage timezone(lib uses japanes time) and latitude
+                const birthInfo = <?php echo json_encode($_SESSION['birthInfo']); ?>
 
-                var allPlanets = new Array();
-                allPlanets = calPlanetPosition2( 2000, 11, 22, 11, 11, 151.2, -33.87 );
-                                        //year, month, day, hour, minute, longitude, latitude 
-                var cuspLongitudes = new Array();
-                cuspLongitudes = calHouseCusp2( 2000, 11, 22, 11, 11, 151.2, -33.87 , 1 ).slice(1);
-                //var cuspsL = cuspLongitudes.slice(1)
+                const allPlanets = calPlanetPosition2(
+                    birthInfo.year, 
+                    birthInfo.month,
+                    birthInfo.day,
+                    birthInfo.hour,
+                    birthInfo.minute,
+                    birthInfo.longitude,
+                    birthInfo.latitude
+                );
 
-                var planetsP = {
+                const cuspLongitudes = calHouseCusp2(
+                    birthInfo.year, 
+                    birthInfo.month,
+                    birthInfo.day,
+                    birthInfo.hour,
+                    birthInfo.minute,
+                    birthInfo.longitude,
+                    birthInfo.latitude,
+                    1 
+                ).slice(1);
+
+                const planetsP = {
                     "Sun":[allPlanets[1]], 
                     "Moon":[allPlanets[2]], 
                     "Mercury":[allPlanets[3]], 
@@ -41,10 +59,9 @@
 
                 const desc = (allPlanets[13] + 180) % 360;//13 is ascending this is getting the descending from it
                 const ic = (allPlanets[14] + 180) % 360;//14 is midheaven, this is getting the imum coeli(opposite thingy) from it
-							      
+
         	    window.onload = function () {   
-				    var radix = new astrochart.Chart('birthChart', 800, 800).radix( data );
-										
+				    var radix = new astrochart.Chart('birthChart', 600, 600).radix( data );
 				    radix.addPointsOfInterest({ As: [allPlanets[13]], Mc: [allPlanets[14]], Ds: [desc], Ic: [ic]});
 				    radix.aspects();	
                 }
--- src/View/loggedIn.php
+++ src/View/loggedIn.php
@@ -3,6 +3,17 @@
         <?php
         session_start();
         $email = $_SESSION['email'];
+        //allPlanets = calPlanetPosition2( 2000, 11, 22, 11, 11, 151.2, -33.87 );
+                                        //year, month, day, hour, minute, longitude, latitude 
+        $_SESSION['birthInfo'] = [
+            'year'  => 2000,
+            'month' => 11,
+            'day' => 22,
+            'hour'  => 11,
+            'minute' => 11,
+            'longitude' => 151.2,
+            'latitude' => -33.87
+        ]
         ?>
         <head>
             <title>You've been logged in!</title> 
