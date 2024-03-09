6dae00d3db456e59e38003d3f796d653857ca22a
Alex <21338787@studentmail.ul.ie>
2024-03-09 13:42:00 +0000

    match score is created

--- src/Model/MatchMaker.php
+++ src/Model/MatchMaker.php
@@ -1,8 +1,26 @@
 <?php
 include "Profile.php";
 class MatchMaker{
-    public function generatePotentialMatches($profile, $seeking){
-        
+
+    public function createMatchScore(
+        String $profile_one_sign,
+        array $profile_one_interests,
+        String $profile_two_sign,
+        array $profile_two_interests
+    ){
+        $matchScore = ($this->getSignScore($profile_one_sign, $profile_two_sign))*2;
+        $matchScore += count(array_intersect($profile_one_interests, $profile_two_interests));
+        return $matchScore;
     }
+
+    public function getSignScore(
+        String $profile_one_sign,
+        String $profile_two_sign
+    ){
+        include "matchScores.php";
+        $signsPair = implode('',sort($profile_one_sign, $profile_two_sign));
+        return $SIGN_MATCH_SCORES[$signsPair];
+    }
+
     
 }
