5691de51ba2f2e73e21f9166bc412d644925fd14
Alex <21338787@studentmail.ul.ie>
2024-03-20 18:56:56 +0000

    add doctype html to all the views

--- src/View/adminConsole.php
+++ src/View/adminConsole.php
@@ -1,18 +1,24 @@
+<!DOCTYPE html>
 <html>
-    <div class="generalText">
-    
+<div class="generalText">
+
     <?php
-      session_start();
-      $email = $_SESSION['email'];
+    session_start();
+    $email = $_SESSION['email'];
     ?>
+
     <head>
-        <title>You've been logged in!</title> 
-        <link href='../style.css' rel='stylesheet'> 
+        <title>You've been logged in!</title>
+        <link href='../style.css' rel='stylesheet'>
     </head>
+
     <body id="loggedBody">
         <div class="loggedPage">
-            <h1>Welcome <?=$email;?></h1>
+            <h1>Welcome
+                <?= $email; ?>
+            </h1>
         </div>
     </body>
-    </div>
+</div>
+
 </html>
