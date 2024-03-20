0e38605b230382b7688a961987f5cb05a13204f1
Alex <21338787@studentmail.ul.ie>
2024-03-20 15:35:31 +0000

    ban func added

--- src/Model/Admin.php
+++ src/Model/Admin.php
@@ -1,32 +1,21 @@
 <?php
     class Admin{
         private $id;
-        private $email;
-        public function __construct($id, $email){
+        public function __construct($id){
             $this->id = $id;
-            $this->email = $email;
-
-            //$this->password = $password
-            //db hash and put into the db
         }
 
         function getId(){
             return $this->id;
         }
 
-        function getEmail(){   
-            return $this->email;
-        }
-
-        function setEmail($email){
-            $this->email = $email;
-        }
-
-        function banUser($userId, $daysBanned){
-            date_default_timezone_set('Ireland/Dublin');
-            //$user = db.getUser($userId);
-            //$user.setBannedUntill(date('Y-m-d', strtotime("+$daysBanned days"))
-            return date('Y-m-d', strtotime("+30 days"));
+        function banUser(int $userId, int $daysBanned, $conn){
+            date_default_timezone_set('utc');
+            $intreval = DateInterval::createFromDateString($daysBanned . "day");
+            $banDate = ((new DateTime())->add($intreval))->format('Y-m-d');
+            $banStr = "UPDATE profile SET `banned_until`='$banDate' WHERE `id`='$userId'";
+            $banStatement = $conn->prepare($banStr);
+            $banStatement->execute();
         }
 
         function editProfile(){
