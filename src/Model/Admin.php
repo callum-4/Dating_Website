0bf1e04c3a053dde30b69629be36811ff710396a
Alex <21338787@studentmail.ul.ie>
2024-03-20 18:46:56 +0000

    login function changed to check for ban status and andmin email changed to refrence astrolove domain

--- src/Model/Admin.php
+++ src/Model/Admin.php
@@ -1,34 +1,40 @@
 <?php
-    class Admin{
-        private $id;
-        public function __construct($id){
-            $this->id = $id;
-        }
-
-        function getId(){
-            return $this->id;
-        }
-
-        function banUser(int $userId, int $daysBanned, $conn){
-            date_default_timezone_set('utc');
-            $intreval = DateInterval::createFromDateString($daysBanned . "day");
-            $banDate = ((new DateTime())->add($intreval))->format('Y-m-d');
-            $banStr = "UPDATE profile SET `banned_until`='$banDate' WHERE `id`='$userId'";
-            $banStatement = $conn->prepare($banStr);
-            $banStatement->execute();
-        }
-
-        function editProfile(){
-            //finnish once profile complete
-        }
-
-        function deleteUser($userId){
-            //db.deleteUser($userId)
-            //db should cascade to all instances
-        }
-
-        function deleteAd($adId){
-            //db.deleteAd($adId)
-        }
+class Admin
+{
+    private $id;
+    public function __construct($id)
+    {
+        $this->id = $id;
     }
-?>
+
+    function getId()
+    {
+        return $this->id;
+    }
+
+    function banUser(int $userId, int $daysBanned, $conn)
+    {
+        date_default_timezone_set('utc');
+        $intreval = DateInterval::createFromDateString($daysBanned . "day");
+        $banDate = ((new DateTime())->add($intreval))->format('Y-m-d');
+        $banStr = "UPDATE profile SET `banned_until`='$banDate' WHERE `id`='$userId'";
+        $banStatement = $conn->prepare($banStr);
+        $banStatement->execute();
+    }
+
+    function editProfile()
+    {
+        //finnish once profile complete
+    }
+
+    function deleteUser($userId)
+    {
+        //db.deleteUser($userId)
+        //db should cascade to all instances
+    }
+
+    function deleteAd($adId)
+    {
+        //db.deleteAd($adId)
+    }
+}
