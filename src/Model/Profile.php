25de0db512618d46fdc479f79d84dd269e243175
Alex <21338787@studentmail.ul.ie>
2024-03-22 11:43:13 +0000

    change dob to datetime inside the profile method and account for that in the get profile sql

--- src/Model/Profile.php
+++ src/Model/Profile.php
@@ -1,5 +1,6 @@
 <?php
-class Profile{
+class Profile
+{
 
     public $id;
     public $email;
@@ -11,21 +12,23 @@ class Profile{
     public $bannedUntil;
     public $zodiacSign;
 
-    function __construct($id, $email, $gender, $name, $dob, $description, $interests, $bannedUntil){
+    function __construct($id, $email, $gender, $name, $dob, $description, $interests, $bannedUntil)
+    {
         $this->id = $id;
         $this->email = $email;
         $this->gender = $gender;
         $this->name = $name;
-        $this->dob = $dob;
+        $this->dob = new Datetime($dob);
         $this->description = $description;
         $this->interests = $this->createInterestsArray($interests);
         $this->bannedUntil = new DateTime('2000-10-10'); //deafult banned until
-        $this->zodiacSign = $this->makeZodiacSign($dob);
+        $this->zodiacSign = $this->makeZodiacSign($this->dob);
     }
 
-    
 
-    function get_profile() {
+
+    function get_profile()
+    {
         return [
             'id' => $this->id,
             'email' => $this->email,
@@ -34,35 +37,40 @@ class Profile{
             'dob' => $this->dob,
             'description' => $this->description,
             'interests' => $this->interests,
-            'bannedUntil'=> $this->bannedUntil,
-            'zodiacSign'=> $this->zodiacSign
+            'bannedUntil' => $this->bannedUntil,
+            'zodiacSign' => $this->zodiacSign
         ];
     }
 
-    function createInterestsArray(string $interests){
-        $interestUntrimmed =  preg_split("/\,/", $interests);
+    function createInterestsArray(string $interests)
+    {
+        $interestUntrimmed = preg_split("/\,/", $interests);
         $interestsTrimmed = array();
-        for($i=0; $i < count($interestUntrimmed); $i++){
+        for ($i = 0; $i < count($interestUntrimmed); $i++) {
             $interestsTrimmed[$i] = trim($interestUntrimmed[$i], " \n\r\t\v\x00,");
         }
         return $interestsTrimmed;
     }
 
-    function getInsertIntrestsQuery(){
+    function getInsertIntrestsQuery()
+    {
         $interestsQuery = 'INSERT INTO `profile_interest` (`profile_id`, `interest`) VALUES ';
         $query_parts = array();
-        for($x=0; $x<count($this->interests); $x++){
+        for ($x = 0; $x < count($this->interests); $x++) {
             $query_parts[] = "('" . $this->id . "', '" . $this->interests[$x] . "')";
         }
         $interestsQuery .= implode(',', $query_parts);
         return $interestsQuery;
     }
 
-    function getUpdateProfileInDBQuery(){
-        return "UPDATE profile SET `name`='$this->name', `gender`='$this->gender', `description`='$this->description',  `datetime_of_birth`='$this->dob' WHERE `id`='$this->id'";
+    function getUpdateProfileInDBQuery()
+    {
+        $dateTimeOB = ($this->dob)->format("Y-m-d H:i:s");
+        return "UPDATE profile SET `name`='$this->name', `gender`='$this->gender', `description`='$this->description',  `datetime_of_birth`='$dateTimeOB' WHERE `id`='$this->id'";
     }
 
-    public function makeZodiacSign(Datetime $datetimeOB){
+    public function makeZodiacSign(Datetime $datetimeOB)
+    {
         $year = '2000';//compare only months and ignore the year
         $day = $datetimeOB->format('m-d');
         $date = new \DateTime("$year-$day");
@@ -70,26 +78,26 @@ class Profile{
             return "Capricorn";
         }
         $zodiacs = array(
-            "Aquarius"=>"2000-02-18",
-            "Pisces"=>"2000-03-20",
-            "Aries"=>"2000-04-19",
-            "Taurus"=>"2000-05-20",
-            "Gemini"=>"2000-06-20",
-            "Cancer"=>"2000-07-22",
-            "Leo"=>"2000-08-22",
-            "Virgo"=>"2000-09-22",
-            "Libra"=>"2000-10-22",
-            "Scorpio"=>"2000-11-21",
-            "Sagittarius"=>"2000-12-21",
-            "Capricorn"=>"2000-12-31",
+            "Aquarius" => "2000-02-18",
+            "Pisces" => "2000-03-20",
+            "Aries" => "2000-04-19",
+            "Taurus" => "2000-05-20",
+            "Gemini" => "2000-06-20",
+            "Cancer" => "2000-07-22",
+            "Leo" => "2000-08-22",
+            "Virgo" => "2000-09-22",
+            "Libra" => "2000-10-22",
+            "Scorpio" => "2000-11-21",
+            "Sagittarius" => "2000-12-21",
+            "Capricorn" => "2000-12-31",
         );
         foreach ($zodiacs as $zodiac => $endDate) {
-            if($date <= new DateTime($endDate)) {
+            if ($date <= new DateTime($endDate)) {
                 return $zodiac;
             }
         }
         return "zodiac could not be determined";
-        
+
     }
 }
 
