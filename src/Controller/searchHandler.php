1093cfefa2fd337d86be3050de9ffa5151ae8611
Alex <21338787@studentmail.ul.ie>
2024-03-23 19:09:38 +0000

    all search feilds functional

--- src/Controller/searchHandler.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once "../View/search.php";
-echo $_POST["searchText"] . "<br>";
-echo $_POST["searchCatagory"] . "awa";
--- src/View/search.php
+++ src/View/search.php
@@ -10,7 +10,7 @@
     </head>
 
     <body>
-        <form method="post" action="../Controller/searchHandler.php">
+        <form method="post" action="">
             <div class="card">
                 <div class="row no-gutters mt-2">
                     <div class="col-1"></div>
@@ -22,7 +22,7 @@
                 </div>
                 <div class="row text-center m-2">
                     <div class="col">
-                        <input type="radio" id="name" name="searchCatagory" value="name">
+                        <input type="radio" id="name" name="searchCatagory" value="name" checked>
                         <label for="name">Name</label>
                     </div>
                     <div class="col">
@@ -36,6 +36,72 @@
                 </div>
             </div>
         </form>
+        <?php
+        require_once "../Controller/console.php";
+
+        if (isset ($_POST)) {
+            $searchBy = $_POST["searchCatagory"];
+            $searchTerm = $_POST["searchText"];
+            $searchQuery = "SELECT * FROM `profile` WHERE ";
+
+            switch ($searchBy) {
+                case "name":
+                    $searchQuery .= "UPPER(`name`) LIKE UPPER('%$searchTerm%')";
+                    //$whereStr = "UPPER(`name`) LIKE UPPER('%$searchTerm%')";
+                    break;
+                case "sign":
+                    $sign = signToDates(strtolower($searchTerm));
+                    if (!isset ($sign)) {
+                        echo "please enter one of the 12 major western zodiacs";
+                        return;
+                    }
+                    $startDate = $sign['start'];
+                    $endDate = $sign['end'];
+
+                    $searchQuery .= "DATE_FORMAT(`datetime_of_birth`, '%m-%d') >= '$startDate' AND DATE_FORMAT(`datetime_of_birth`, '%m-%d') <= '$endDate'";
+                    break;
+                case "interest":
+                    $searchQuery = "SELECT profile.* FROM profile WHERE `id` IN(
+                        SELECT profile_interest.profile_id FROM `profile_interest` WHERE `interest` = '$searchTerm')";
+            }
+            try {
+                $conn = mysqli_connect("localhost", "callum", "test123", "dating website");
+
+                //$searchQuery = "SELECT * FROM `profile` WHERE " . $whereStr;
+                console_log($searchQuery);
+                $result = mysqli_query($conn, $searchQuery);
+                $fetched_profiles = mysqli_fetch_all($result);
+                console_log($fetched_profiles);
+                foreach ($fetched_profiles as $p) {
+                    print_r($p);
+                    print "<br>";
+                }
+            } catch (Exception $e) {
+                console_log($e->getMessage());
+            }
+
+        }
+
+        function signToDates(string $sign)
+        {
+            $zodiacs = array(
+                'aries' => array('start' => '03-21', 'end' => '04-19'),
+                'taurus' => array('start' => '04-20', 'end' => '05-20'),
+                'gemini' => array('start' => '05-21', 'end' => '06-20'),
+                'cancer' => array('start' => '06-21', 'end' => '07-22'),
+                'leo' => array('start' => '07-23', 'end' => '08-22'),
+                'virgo' => array('start' => '08-23', 'end' => '09-22'),
+                'libra' => array('start' => '09-23', 'end' => '10-22'),
+                'scorpio' => array('start' => '10-23', 'end' => '11-21'),
+                'sagittarius' => array('start' => '11-22', 'end' => '12-21'),
+                'capricorn' => array('start' => '12-22', 'end' => '01-19'),
+                'aquarius' => array('start' => '01-20', 'end' => '02-18'),
+                'pisces' => array('start' => '02-19', 'end' => '03-20')
+            );
+            return $zodiacs[$sign];
+        }
+        ?>
+
     </body>
 
 </html>
